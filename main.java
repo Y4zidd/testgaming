b9797a48ee29ec344cea0bc152b29f7d2d06ed02
Y4zid <dahlahg7@gmail.com>
2025-04-17 11:55:12 +0700

    assesment 3

--- main.java
+++ main.java
@@ -1,3 +1,22 @@
 public class main {
     //dfs anda bsf
+    public static void main(String[] args) {
+        // Create a graph with 5 vertices
+        Graph graph = new Graph(5);
+
+        // Add edges to the graph
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+
+        // Perform DFS starting from vertex 0
+        System.out.println("Depth First Search (DFS) starting from vertex 0:");
+        graph.dfs(0);
+
+        // Perform BFS starting from vertex 0
+        System.out.println("\nBreadth First Search (BFS) starting from vertex 0:");
+        graph.bfs(0);
+    }
 }
